d83e2c45ba8d56d2c2342c31df48312bdda8566e
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2024-01-29 11:14:40 +0300

    add 13-buttons

---
+++ 13-buttons/app.js
@@ -0,0 +1,32 @@
+const wrapper = document.querySelector('.wrapper');
+
+let counter = 0;
+
+const count = document.createElement('div');
+count.innerHTML = `Число нажатий: ${counter}`;
+count.setAttribute('style', 'font-size: 25px;');
+document.body.append(count);
+
+for (let i = 0; i < 5; ++i) {
+	const el = document.createElement('button');
+	el.innerHTML = 'Нажми меня';
+	el.setAttribute(
+		'style',
+		'width: 150px; height: 45px; background-color: #f7df1e; color: #1c1b21; border: 1px solid black; border-radius: 15px; box-shadow: 5px 4px 3px rgb(255, 227, 0);'
+	);
+	el.addEventListener('click', () => {
+		resetButtons();
+		counter++;
+		count.innerHTML = `Число нажатий: ${counter}`;
+		el.innerHTML = 'Нажато';
+		el.style.boxShadow = 'none';
+	});
+	wrapper.append(el);
+}
+
+function resetButtons() {
+	document.querySelectorAll('button').forEach((button) => {
+		button.innerHTML = 'Нажми меня';
+		button.style.boxShadow = '5px 4px 3px rgb(255, 227, 0)';
+	});
+}
