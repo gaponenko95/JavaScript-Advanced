12b02794be13c6a2f93c581fae2db883a21c7424
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2024-02-04 16:58:42 +0300

    fix app.js in 9-pokemon

--- 9-pokemon/app.js
+++ 9-pokemon/app.js
@@ -5,12 +5,20 @@ request.send();
 request.addEventListener('load', function () {
 	const { abilities } = JSON.parse(this.responseText);
 
-	const request = new XMLHttpRequest();
-	request.open('GET', abilities[0].ability.url);
-	request.send();
+	const abilityRequest = new XMLHttpRequest();
+	abilityRequest.open('GET', abilities[0].ability.url);
+	abilityRequest.send();
 
-	request.addEventListener('load', function () {
+	abilityRequest.addEventListener('load', function () {
 		const { effect_entries } = JSON.parse(this.responseText);
 		console.log(effect_entries[1].effect);
 	});
+
+	abilityRequest.addEventListener('error', function () {
+		console.error('Failed to load ability data!');
+	});
+});
+
+request.addEventListener('error', function () {
+	console.error('Failed to load Pokemon data!');
 });
