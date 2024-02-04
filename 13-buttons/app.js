afc79983175dd1eac6f4613d893d2f04e3d3d282
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2024-02-04 17:35:33 +0300

    fix app.js in 13-buttons

--- 13-buttons/app.js
+++ 13-buttons/app.js
@@ -14,18 +14,21 @@ for (let i = 0; i < 5; ++i) {
 		'style',
 		'width: 150px; height: 45px; background-color: #f7df1e; color: #1c1b21; border: 1px solid black; border-radius: 15px; box-shadow: 5px 4px 3px rgb(255, 227, 0);'
 	);
-	el.addEventListener('click', () => {
-		resetButtons();
-		counter++;
-		count.innerHTML = `Число нажатий: ${counter}`;
-		el.innerHTML = 'Нажато';
-		el.style.boxShadow = 'none';
-	});
 	wrapper.append(el);
 }
 
+wrapper.addEventListener('click', (event) => {
+	if (event.target.tagName === 'button') {
+		resetButtons();
+		counter++;
+		count.innerHTML = `Число нажатий: ${counter}`;
+		event.target.innerHTML = 'Нажато';
+		event.target.style.boxShadow = 'none';
+	}
+});
+
 function resetButtons() {
-	document.querySelectorAll('button').forEach((button) => {
+	Array.from(wrapper.children).forEach((button) => {
 		button.innerHTML = 'Нажми меня';
 		button.style.boxShadow = '5px 4px 3px rgb(255, 227, 0)';
 	});
